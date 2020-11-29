59b79fce31fedea3a55aea050ad35f2aa4e4e38b
github-action-benchmark <github@users.noreply.github.com>
2020-11-29 10:24:33 +0000

    add rt_multi_threaded (cargo) benchmark result for c55d846f4b248b4a72335d6c57829fa6396ab9a5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1606645464632,
+  "lastUpdate": 1606645473598,
   "repoUrl": "https://github.com/timbrehifi/tokio",
   "entries": {
     "sync_semaphore": [
@@ -1123,6 +1123,54 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 22536569,
             "range": "± 2844498",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alice@ryhl.io",
+            "name": "Alice Ryhl",
+            "username": "Darksonn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c55d846f4b248b4a72335d6c57829fa6396ab9a5",
+          "message": "util: add rt to tokio-util full feature (#3194)",
+          "timestamp": "2020-11-29T09:48:31+01:00",
+          "tree_id": "5f27b29cd1018796f0713d6e87e4823920ba5084",
+          "url": "https://github.com/timbrehifi/tokio/commit/c55d846f4b248b4a72335d6c57829fa6396ab9a5"
+        },
+        "date": 1606645468849,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 166464,
+            "range": "± 22489",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 708599,
+            "range": "± 96826",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 5281395,
+            "range": "± 771098",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 19719670,
+            "range": "± 2614883",
             "unit": "ns/iter"
           }
         ]
