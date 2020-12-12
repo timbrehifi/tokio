aff3eab4014ca39609560021f7eb37b1812d4c4e
github-action-benchmark <github@users.noreply.github.com>
2020-12-12 17:20:58 +0000

    add rt_multi_threaded (cargo) benchmark result for df20c162ae1308c07073b6a67c8ba4202f52d208

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1607752099957,
+  "lastUpdate": 1607793658743,
   "repoUrl": "https://github.com/timbrehifi/tokio",
   "entries": {
     "sync_semaphore": [
@@ -3571,6 +3571,54 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 18909808,
             "range": "± 2725182",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sunjay@users.noreply.github.com",
+            "name": "Sunjay Varma",
+            "username": "sunjay"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "df20c162ae1308c07073b6a67c8ba4202f52d208",
+          "message": "sync: add blocking_recv method to UnboundedReceiver, similar to Receiver::blocking_recv (#3262)",
+          "timestamp": "2020-12-12T08:47:35-08:00",
+          "tree_id": "94fe5abd9735b0c4985d5b38a8d96c51953b0f0b",
+          "url": "https://github.com/timbrehifi/tokio/commit/df20c162ae1308c07073b6a67c8ba4202f52d208"
+        },
+        "date": 1607793657712,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 156794,
+            "range": "± 34070",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 610276,
+            "range": "± 91952",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 4584315,
+            "range": "± 908378",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 17310668,
+            "range": "± 2570129",
             "unit": "ns/iter"
           }
         ]
