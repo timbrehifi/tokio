f2382061d230f61e153b1edbefb4d1a2fa9fc60e
github-action-benchmark <github@users.noreply.github.com>
2020-12-27 12:42:01 +0000

    add sync_rwlock (cargo) benchmark result for 770044caa70465fbe5d2851a8d9a0bc0e5e7429a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1609072912110,
+  "lastUpdate": 1609072921367,
   "repoUrl": "https://github.com/timbrehifi/tokio",
   "entries": {
     "sync_semaphore": [
@@ -8271,6 +8271,60 @@ window.BENCHMARK_DATA = {
             "name": "read_uncontended",
             "value": 552,
             "range": "± 94",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "te316e89@gmail.com",
+            "name": "Taiki Endo",
+            "username": "taiki-e"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "770044caa70465fbe5d2851a8d9a0bc0e5e7429a",
+          "message": "chore: replace deprecated compare_and_swap with compare_exchange (#3331)",
+          "timestamp": "2020-12-27T10:44:21+01:00",
+          "tree_id": "369bb6894ddb442e95e915a873daac756c79a81f",
+          "url": "https://github.com/timbrehifi/tokio/commit/770044caa70465fbe5d2851a8d9a0bc0e5e7429a"
+        },
+        "date": 1609072919760,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "read_concurrent_contended",
+            "value": 1022,
+            "range": "± 287",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_contended_multi",
+            "value": 15732,
+            "range": "± 4842",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_uncontended",
+            "value": 1040,
+            "range": "± 105",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_uncontended_multi",
+            "value": 15764,
+            "range": "± 8022",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_uncontended",
+            "value": 572,
+            "range": "± 84",
             "unit": "ns/iter"
           }
         ]
