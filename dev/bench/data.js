73d85819d167e75ef1bc25d59ccd462471e71613
github-action-benchmark <github@users.noreply.github.com>
2021-01-01 13:11:13 +0000

    add sync_mpsc (cargo) benchmark result for 36918e05e567f53e0a9cf12b647d6bb34e0b9e8d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1609506667875,
+  "lastUpdate": 1609506673403,
   "repoUrl": "https://github.com/timbrehifi/tokio",
   "entries": {
     "sync_semaphore": [
@@ -13199,6 +13199,90 @@ window.BENCHMARK_DATA = {
             "name": "uncontented_unbounded",
             "value": 828673,
             "range": "± 111213",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tesaguriguma@gmail.com",
+            "name": "Daiki Mizukami",
+            "username": "tesaguri"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "36918e05e567f53e0a9cf12b647d6bb34e0b9e8d",
+          "message": "util: add more get_{ref,mut,pin_mut} methods (#3364)\n\nThis commit adds:\r\n\r\n- Framed::get_pin_mut\r\n- FramedRead::get_pin_mut\r\n- FramedWrite::get_pin_mut\r\n- StreamReader::get_ref\r\n- StreamReader::get_mut\r\n- StreamReader::get_pin_mut\r\n- StreamReader::into_inner",
+          "timestamp": "2021-01-01T13:05:15+01:00",
+          "tree_id": "ca9828a10c320fd13251c15363ec1886381355ae",
+          "url": "https://github.com/timbrehifi/tokio/commit/36918e05e567f53e0a9cf12b647d6bb34e0b9e8d"
+        },
+        "date": 1609506672441,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "contention_bounded",
+            "value": 6775740,
+            "range": "± 2282893",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_bounded_full",
+            "value": 6717579,
+            "range": "± 2045785",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contention_unbounded",
+            "value": 6276002,
+            "range": "± 2494788",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_000_medium",
+            "value": 660,
+            "range": "± 7",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_100_medium",
+            "value": 625,
+            "range": "± 20",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "create_1_medium",
+            "value": 621,
+            "range": "± 38",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_large",
+            "value": 84492,
+            "range": "± 4619",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "send_medium",
+            "value": 1437,
+            "range": "± 118",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_bounded",
+            "value": 1128985,
+            "range": "± 55164",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontented_unbounded",
+            "value": 807234,
+            "range": "± 41098",
             "unit": "ns/iter"
           }
         ]
