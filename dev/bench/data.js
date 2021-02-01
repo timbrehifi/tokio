eaa8566c20e39855172bb565a35b253c3d1c04c5
github-action-benchmark <github@users.noreply.github.com>
2021-02-01 18:07:29 +0000

    add rt_multi_threaded (cargo) benchmark result for cc97fb8a5ff6ef17f6dc9968fbbe67002cff4130

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612202844967,
+  "lastUpdate": 1612202849368,
   "repoUrl": "https://github.com/timbrehifi/tokio",
   "entries": {
     "sync_semaphore": [
@@ -9493,6 +9493,54 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 18781877,
             "range": "± 3287400",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "artem@vorotnikov.me",
+            "name": "Artem Vorotnikov",
+            "username": "vorot93"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cc97fb8a5ff6ef17f6dc9968fbbe67002cff4130",
+          "message": "Suppress deprecated warnings for spin_loop_hint (#3497)\n\n* Suppress deprecated warnings for spin_loop_hint\n\n* Update tokio/src/loom/std/mod.rs\n\nCo-authored-by: Taiki Endo <te316e89@gmail.com>\n\n* Update tokio/src/sync/task/atomic_waker.rs\n\nCo-authored-by: Taiki Endo <te316e89@gmail.com>\n\n* fmt\n\nCo-authored-by: Taiki Endo <te316e89@gmail.com>",
+          "timestamp": "2021-02-01T15:55:21+03:00",
+          "tree_id": "effd8bf970f9044cf0ebe0b3cd8446b297b3efc8",
+          "url": "https://github.com/timbrehifi/tokio/commit/cc97fb8a5ff6ef17f6dc9968fbbe67002cff4130"
+        },
+        "date": 1612202848200,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 198614,
+            "range": "± 55378",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 731917,
+            "range": "± 157745",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 5340883,
+            "range": "± 1328171",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 20262127,
+            "range": "± 2537547",
             "unit": "ns/iter"
           }
         ]
