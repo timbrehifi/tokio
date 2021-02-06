f6b1ffcf8d1c2e8e6d31773135fb634551de2b4e
github-action-benchmark <github@users.noreply.github.com>
2021-02-06 19:18:05 +0000

    add sync_rwlock (cargo) benchmark result for 6fd9084d470aa34e02c9dedcbf52e310fcb9cacc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612639078078,
+  "lastUpdate": 1612639085144,
   "repoUrl": "https://github.com/timbrehifi/tokio",
   "entries": {
     "sync_semaphore": [
@@ -14979,6 +14979,60 @@ window.BENCHMARK_DATA = {
             "name": "read_uncontended",
             "value": 609,
             "range": "± 62",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alice@ryhl.io",
+            "name": "Alice Ryhl",
+            "username": "Darksonn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6fd9084d470aa34e02c9dedcbf52e310fcb9cacc",
+          "message": "runtime: add link to LocalSet on new_current_thread (#3508)",
+          "timestamp": "2021-02-06T19:08:59+01:00",
+          "tree_id": "77a10fa30c4ca6c048ed608bcb67ef74645cac27",
+          "url": "https://github.com/timbrehifi/tokio/commit/6fd9084d470aa34e02c9dedcbf52e310fcb9cacc"
+        },
+        "date": 1612639083917,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "read_concurrent_contended",
+            "value": 986,
+            "range": "± 148",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_contended_multi",
+            "value": 18758,
+            "range": "± 6681",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_uncontended",
+            "value": 1122,
+            "range": "± 117",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_concurrent_uncontended_multi",
+            "value": 20427,
+            "range": "± 10185",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_uncontended",
+            "value": 597,
+            "range": "± 51",
             "unit": "ns/iter"
           }
         ]
