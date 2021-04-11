f448221ff5d03c798d2b924f4b679f1bfaec17a8
github-action-benchmark <github@users.noreply.github.com>
2021-04-11 21:51:20 +0000

    add sync_semaphore (cargo) benchmark result for 28d68798973fbfaab889d73c4da7cbc2d5a0e2f8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618177851979,
+  "lastUpdate": 1618177880785,
   "repoUrl": "https://github.com/timbrehifi/tokio",
   "entries": {
     "sync_semaphore": [
@@ -8099,6 +8099,60 @@ window.BENCHMARK_DATA = {
             "name": "uncontended_concurrent_single",
             "value": 1045,
             "range": "± 118",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "david.pdrsn@gmail.com",
+            "name": "David Pedersen",
+            "username": "davidpdrsn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "28d68798973fbfaab889d73c4da7cbc2d5a0e2f8",
+          "message": "macros: forward input arguments in `#[tokio::test]` (#3691)\n\nFixes #2388\r\n\r\nPreviously `#[tokio::test]` would error on functions that took\r\narguments. That meant other attribute macros couldn't do further\r\ntransformations on them. This changes that so arguments are forwarded as\r\nis.\r\n\r\nWhatever else might be included on the function is forwarded as well.\r\nFor example return type, generics, etc.\r\n\r\nWorth noting that this is only for compatibility with other macros.\r\n`#[test]`s that take arguments will still fail to compile.\r\n\r\nA bit odd that [trybuild] tests don't fail `#[test]` functions with\r\narguments which is why the new tests are run with `t.pass(...)`. They do\r\nactually fail if part of a real crate.\r\n\r\n[trybuild]: https://crates.io/crates/trybuild",
+          "timestamp": "2021-04-11T20:39:05+02:00",
+          "tree_id": "c50965c227bfc40bef8ff305d46f85318e04397d",
+          "url": "https://github.com/timbrehifi/tokio/commit/28d68798973fbfaab889d73c4da7cbc2d5a0e2f8"
+        },
+        "date": 1618177878052,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "contended_concurrent_multi",
+            "value": 13920,
+            "range": "± 3043",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "contended_concurrent_single",
+            "value": 1026,
+            "range": "± 102",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontended",
+            "value": 626,
+            "range": "± 98",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontended_concurrent_multi",
+            "value": 14696,
+            "range": "± 5496",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "uncontended_concurrent_single",
+            "value": 1087,
+            "range": "± 117",
             "unit": "ns/iter"
           }
         ]
