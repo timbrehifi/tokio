865078cb6146521b488c5a14309a05162e4cb111
github-action-benchmark <github@users.noreply.github.com>
2021-04-15 21:46:34 +0000

    add rt_multi_threaded (cargo) benchmark result for d6da67b2e612455e16c11967c762ae802c2ac428

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1618523165800,
+  "lastUpdate": 1618523193938,
   "repoUrl": "https://github.com/timbrehifi/tokio",
   "entries": {
     "sync_semaphore": [
@@ -15913,6 +15913,54 @@ window.BENCHMARK_DATA = {
             "name": "yield_many",
             "value": 20959043,
             "range": "± 3417554",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "eliza@buoyant.io",
+            "name": "Eliza Weisman",
+            "username": "hawkw"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d6da67b2e612455e16c11967c762ae802c2ac428",
+          "message": "sync: add `mpsc::Sender::{reserve_owned, try_reserve_owned}` (#3704)\n\n* sync: add `mpsc::Sender::{reserve_owned, try_reserve_owned}`\r\n\r\n## Motivation\r\n\r\nThe `mpsc::Sender::reserve` method currently returns a permit that borrows\r\nfrom the `Sender`. It would be nice to have a version of it that returns\r\nan owned permit.\r\n\r\n## Solution\r\n\r\nThis branch adds an `OwnedPermit` type and `Sender::{reserve_owned,\r\ntry_reserve_owned}` methods. Unlike the comparable methods on\r\n`Semaphore`, these methods do *not* require an `Arc<Sender>` as the\r\nreceiver; this is because the sender internally reference counts the\r\nchannel and is already cheap to clone. Requiring an `Arc` would simply\r\nadd an unnecessary second layer of reference counting, which is not\r\nideal; instead, the documentation encourages the user to clone the\r\nsender prior to calling `reserve_owned` when necessary.\r\n\r\nSince these methods take the `Sender` by value, they also have the ability\r\nto _return_ the `Sender` from a successful `OwnedPermit::send`. This\r\nallows them to be used without additional clones. Essentially, this is a\r\nvery simple type-level encoding of the sender's state, with the\r\ntransitions\r\n```\r\n      ┌──────┐\r\n ┌───►│Sender├───┐\r\n │    └──────┘   │\r\nsend          reserve\r\n │ ┌───────────┐ │\r\n └─┤OwnedPermit│◄┘\r\n   └───────────┘\r\n```\r\n\r\nAdditionally, I added an `OwnedPermit::release`, which returns the\r\n`Sender` and releases the permit *without* sending a message.\r\n\r\nCloses #3688 \r\n\r\nSigned-off-by: Eliza Weisman <eliza@buoyant.io>",
+          "timestamp": "2021-04-15T12:55:57-07:00",
+          "tree_id": "bf29d9dc513a8d80cadd15021e58c979e5f6d691",
+          "url": "https://github.com/timbrehifi/tokio/commit/d6da67b2e612455e16c11967c762ae802c2ac428"
+        },
+        "date": 1618523191959,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 204084,
+            "range": "± 46517",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 736653,
+            "range": "± 105490",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 5251672,
+            "range": "± 1425235",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 20312356,
+            "range": "± 4820550",
             "unit": "ns/iter"
           }
         ]
